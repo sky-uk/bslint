0b9620a9d79172f7ed1bfd04f025dc894b154aaa
Shane Bloomer <shane.bloomer@sky.uk>
2016-08-18 09:54:22 +0100

    Added tests for the reserved keywords

--- src/regexs.py
+++ src/regexs.py
@@ -35,6 +35,25 @@ List = [
     ("<", const.STMT),
     (">", const.STMT),
     ("'", const.STMT),
+
+    ("FOR EACH", const.STMT),
+    ("END FOR", const.STMT),
+    ("ELSE IF", const.STMT),
+    ("EXIT FOR", const.STMT),
+    ("END IF", const.STMT),
+    ("END WHILE", const.STMT),
+    ("EXIT WHILE", const.STMT),
+    ("END FOR", const.STMT),
+    ("IF", const.STMT),
+    ("THEN", const.STMT),
+    ("ELSE", const.STMT),
+    ("FOR", const.STMT),
+    ("TO", const.STMT),
+    ("END", const.STMT),
+    ("STEP", const.STMT),
+    ("IN", const.STMT),
+    ("WHILE", const.STMT),
+
     (r"(^[a-z_][a-z0-9_]*)", const.ID),
     ('\"(.*)\"', const.STRING)
 ]
