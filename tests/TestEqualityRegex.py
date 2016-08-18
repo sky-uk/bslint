f6b651d8bc2ec7a321d0d2d25455890c01c7fa53
danielesassoli <daniele.sassoli@sky.uk>
2016-08-18 12:13:17 +0100

    Refactored lexer and tests accordingly

--- tests/TestEqualityRegex.py
+++ tests/TestEqualityRegex.py
@@ -4,74 +4,53 @@ import src
 
 class TestEqualityMethods(unittest.TestCase):
 
+    def setUp(self):
+        self.lexer = src.Lexer()
+        
+    def testEquals(self):
+        identifier = "=="
+        exp_result = [('==', 'STMT')]
+        result = self.lexer.lex(identifier)
+        self.assertEquals(result, exp_result)
+
     def testNotEquals(self):
         identifier = "<>"
         exp_result = [('<>', 'STMT')]
-        result = src.lexer(identifier)
+        result = self.lexer.lex(identifier)
         self.assertEquals(result, exp_result)
 
     def testLessThanOrEqual(self):
         identifier = "<="
         exp_result = [('<=', 'STMT')]
-        result = src.lexer(identifier)
+        result = self.lexer.lex(identifier)
         self.assertEquals(result, exp_result)
 
     def testLessThanOrEqualSec(self):
         identifier = "=<"
         exp_result = [('=<', 'STMT')]
-        result = src.lexer(identifier)
+        result = self.lexer.lex(identifier)
         self.assertEquals(result, exp_result)
 
     def testGreaterThanOrEqual(self):
         identifier = ">="
         exp_result = [('>=', 'STMT')]
-        result = src.lexer(identifier)
+        result = self.lexer.lex(identifier)
         self.assertEquals(result, exp_result)
 
     def testGreaterThanOrEqualSec(self):
         identifier = "=>"
         exp_result = [('=>', 'STMT')]
-        result = src.lexer(identifier)
+        result = self.lexer.lex(identifier)
         self.assertEquals(result, exp_result)
 
     def testGreaterThan(self):
         identifier = ">"
         exp_result = [('>', 'STMT')]
-        result = src.lexer(identifier)
+        result = self.lexer.lex(identifier)
         self.assertEquals(result, exp_result)
 
     def testLessThan(self):
         identifier = "<"
         exp_result = [('<', 'STMT')]
-        result = src.lexer(identifier)
+        result = self.lexer.lex(identifier)
         self.assertEquals(result, exp_result)
-
-    def testLessThan(self):
-        identifier = "<"
-        exp_result = [('<', 'STMT')]
-        result = src.lexer(identifier)
-        self.assertEquals(result, exp_result)
-
-    def testMOD(self):
-        identifier = "MOD"
-        exp_result = [('MOD', 'STMT')]
-        result = src.lexer(identifier)
-        self.assertEqual(result, exp_result)
-
-    def testNOT(self):
-        identifier = "NOT"
-        exp_result = [('NOT', 'STMT')]
-        result = src.lexer(identifier)
-        self.assertEqual(result, exp_result)
-
-    def testAND(self):
-        identifier = "AND"
-        exp_result = [('AND', 'STMT')]
-        result = src.lexer(identifier)
-        self.assertEqual(result, exp_result)
-
-    def testOR(self):
-        identifier = "OR"
-        exp_result = [('OR', 'STMT')]
-        result = src.lexer(identifier)
-        self.assertEqual(result, exp_result)
--- tests/TestIdentifierRegex.py
+++ tests/TestIdentifierRegex.py
@@ -4,50 +4,53 @@ import src
 
 class TestIdentifierMethods(unittest.TestCase):
 
+    def setUp(self):
+        self.lexer = src.Lexer()
+        
     def testBasicIdentifier(self):
         identifier = "testId"
         exp_result = [('testId', 'ID')]
-        result = src.lexer(identifier)
+        result = self.lexer.lex(identifier)
         self.assertEqual(result, exp_result)
 
     def testIdentifierWithUnderscore(self):
         identifier = "test_Id"
         exp_result = [('test_Id', 'ID')]
-        result = src.lexer(identifier)
+        result = self.lexer.lex(identifier)
         self.assertEqual(result, exp_result)
 
     def testIdentifierStartingWithUnderscore(self):
         identifier = "_testId"
         exp_result = [('_testId', 'ID')]
-        result = src.lexer(identifier)
+        result = self.lexer.lex(identifier)
         self.assertEqual(result, exp_result)
 
     def testIdentifierWithNumbersNotStart(self):
         identifier = "test123ID"
         exp_result = [('test123ID', 'ID')]
-        result = src.lexer(identifier)
+        result = self.lexer.lex(identifier)
         self.assertEqual(result, exp_result)
 
     def testOneLetterIdentifier(self):
         identifier = "t"
         exp_result = [('t', 'ID')]
-        result = src.lexer(identifier)
+        result = self.lexer.lex(identifier)
         self.assertEqual(result, exp_result)
 
     def testIdentifierInStatementWithSpace(self):
         identifier = "_testId ="
         exp_result = ('_testId', 'ID')
-        result = src.lexer(identifier)
+        result = self.lexer.lex(identifier)
         self.assertEqual(result[0], exp_result)
 
     def testIdentifierInStatement(self):
         identifier = "_testId$="
         exp_result = ('_testId', 'ID')
-        result = src.lexer(identifier)
+        result = self.lexer.lex(identifier)
         self.assertEqual(result[0], exp_result)
 
     def testIdentifierAsUnderscore(self):
         identifier = "_"
         exp_result = ('_', 'ID')
-        result = src.lexer(identifier)
+        result = self.lexer.lex(identifier)
         self.assertEqual(result[0], exp_result)
