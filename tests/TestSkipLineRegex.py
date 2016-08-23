4eedd86b27ef76b8f573abf3bc0d047be5cd5669
JackIngleton <jack.ingleton@sky.uk>
2016-08-23 17:05:04 +0100

    added BSLINT comment funtionality

---
+++ tests/TestSkipLineRegex.py
@@ -0,0 +1,39 @@
+import unittest
+import src
+import resources.Constants as const
+
+
+class TestSkipLine(unittest.TestCase):
+    def setUp(self):
+        self.lexer = src.Lexer()
+
+    def testSkipLineSingleSpace(self):
+        identifier = "'  BSLINT_skipline\n"
+        exp_result = [("BSLINT_skipline", const.BSLINT_COMMAND)]
+        result = self.lexer.lex(identifier)
+        self.assertEquals(result, exp_result)
+
+    def testSkipLineNoSpaces(self):
+        identifier = "'BSLINT_skipline\n"
+        exp_result = [("BSLINT_skipline", const.BSLINT_COMMAND)]
+        result = self.lexer.lex(identifier)
+        self.assertEquals(result, exp_result)
+
+    # Should be interpreted as a string
+    def testSkipLineText(self):
+        identifier = "' BSLINT_skipline  random text\n"
+        exp_result = [("BSLINT_skipline", const.BSLINT_COMMAND)]
+        result = self.lexer.lex(identifier)
+        self.assertEquals(result, exp_result)
+
+    def testSkipLineFiveSpaces(self):
+        identifier = "'     BSLINT_skipline\n"
+        exp_result = [("BSLINT_skipline", const.BSLINT_COMMAND)]
+        result = self.lexer.lex(identifier)
+        self.assertEquals(result, exp_result)
+
+    def testSkipLineWithText(self):
+        identifier = "' randomText BSLINT_skipline \n"
+        exp_result = []
+        result = self.lexer.lex(identifier)
+        self.assertEquals(result, exp_result)
