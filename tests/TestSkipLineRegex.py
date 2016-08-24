742af4b8f7a2910448ee159d6c05d9399829c5b6
JackIngleton <jack.ingleton@sky.uk>
2016-08-24 10:19:22 +0100

    Resolved merge conflicts and fixed tests

--- tests/TestSkipLineRegex.py
+++ tests/TestSkipLineRegex.py
@@ -9,26 +9,26 @@ class TestSkipLine(unittest.TestCase):
 
     def testSkipLineSingleSpace(self):
         identifier = "'  BSLINT_skipline"
-        exp_result = [("BSLINT", const.BSLINT_COMMAND)]
+        exp_result = [("BSLINT", const.BSLINT_COMMAND, 1)]
         result = self.lexer.lex(identifier)
         self.assertEquals(result, exp_result)
 
     def testSkipLineNoSpaces(self):
         identifier = "'BSLINT_skipline"
-        exp_result = [("BSLINT", const.BSLINT_COMMAND)]
+        exp_result = [("BSLINT", const.BSLINT_COMMAND, 1)]
         result = self.lexer.lex(identifier)
         self.assertEquals(result, exp_result)
 
     # Should be interpreted as a string
     def testSkipLineText(self):
         identifier = "' BSLINT_skipline"
-        exp_result = [("BSLINT", const.BSLINT_COMMAND)]
+        exp_result = [("BSLINT", const.BSLINT_COMMAND, 1)]
         result = self.lexer.lex(identifier)
         self.assertEquals(result, exp_result)
 
     def testSkipLineFiveSpaces(self):
         identifier = "'     BSLINT_skipline"
-        exp_result = [("BSLINT", const.BSLINT_COMMAND)]
+        exp_result = [("BSLINT", const.BSLINT_COMMAND, 1)]
         result = self.lexer.lex(identifier)
         self.assertEquals(result, exp_result)
 
