e9dd1fdadb8678260af9a4f1f479de84a6961f61
JackIngleton <jack.ingleton@sky.uk>
2016-08-24 13:46:42 +0100

    made BSLINT_command execution generic

---
+++ src/BSLINT_commands/SkipLineCommand.py
@@ -0,0 +1,8 @@
+import re
+
+
+class SkipLineCommand(object):
+
+    @staticmethod
+    def execute():
+        return ""
--- src/__init__.py
+++ src/__init__.py
@@ -1,3 +1,3 @@
 from .Lexer import Lexer
 from .Main import main
-from .ConfigLoader import *
+from .BSLINT_commands.SkipLineCommand import SkipLineCommand
