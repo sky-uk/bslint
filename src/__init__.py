ed583168940f04234d9217c9b9f1a3bf908c3fcb
Zac Robinson <zachary.robinson@sky.uk>
2016-08-25 12:31:38 +0100

    Added syntax checking for comments and refactored what lexer returns

--- src/__init__.py
+++ src/__init__.py
@@ -1,4 +1,5 @@
 from .Lexer import Lexer
 from .Main import main
 from .ConfigLoader import *
-from .BSLINT_commands.SkipLineCommand import SkipLineCommand
+from .BSLINT_commands.SkipLineCommand import *
+from .BSLINT_commands.CheckCommentCommand import *
