e6f65ad1e6d00f16b9ced674ed63a2fbc76113b1
JackIngleton <jack.ingleton@sky.uk>
2016-08-26 10:14:07 +0100

    Refactored comment checking for more customisation

--- tests/TestStyleCheck.py
+++ tests/TestStyleCheck.py
@@ -12,7 +12,7 @@ class TestStyleCheck(unittest.TestCase):
         else:
             cls.filepath_prefix = "../resources/"
 
-    def testDefaultConfigAllStyles(self):
+    def testTODONoComment(self):
         config = src.load_config_file()
         self.lexer = src.Lexer(config)
         file_name = self.filepath_prefix + "ValidCommentSingleQuoteNoTODO.txt"
@@ -32,13 +32,33 @@ class TestStyleCheck(unittest.TestCase):
         result = self.lexer.lex(file)
         self.assertEqual(result["Warnings"], exp_res)
 
-    def testCommentCheck(self):
-        config = src.load_config_file("../resources/config/comment-check-config.json")
+    def testTODOComment(self):
+        config = src.load_config_file("../resources/config/comment-check-TODO-check-config.json")
         self.lexer = src.Lexer(config)
         file_name = self.filepath_prefix + "ValidCommentSingleQuoteNoTODO.txt"
         file = src.main(file_name)
-        exp_res = ['Comments must be TODOs and must be initialled. Line number: 11',
-                   'Comments must be TODOs and must be initialled. Line number: 12',
-                   'Comments must be TODOs and must be initialled. Line number: 17']
+        exp_res = ['TODOs must be initialled. Line number: 17']
+        result = self.lexer.lex(file)
+        self.assertEqual(result["Warnings"], exp_res)
+
+    def testNoTODOComment(self):
+        config = src.load_config_file("../resources/config/comment-check-no-TODO-config.json")
+        self.lexer = src.Lexer(config)
+        file_name = self.filepath_prefix + "ValidCommentSingleQuoteNoTODO.txt"
+        file = src.main(file_name)
+        exp_res = ['Comments must not be TODOs. Line number: 1',
+                   'Comments must not be TODOs. Line number: 17']
+        result = self.lexer.lex(file)
+        self.assertEqual(result["Warnings"], exp_res)
+
+    def testNoTODONoComment(self):
+        config = src.load_config_file("../resources/config/no-comment-no-TODO-config.json")
+        self.lexer = src.Lexer(config)
+        file_name = self.filepath_prefix + "ValidCommentSingleQuoteNoTODO.txt"
+        file = src.main(file_name)
+        exp_res = ['Comments are not allowed. Line number: 1',
+                   'Comments are not allowed. Line number: 11',
+                   'Comments are not allowed. Line number: 12',
+                   'Comments are not allowed. Line number: 17']
         result = self.lexer.lex(file)
         self.assertEqual(result["Warnings"], exp_res)
