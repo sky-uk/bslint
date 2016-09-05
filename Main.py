8890de2cc5837c9f792bbbd4412f4e8b8c3faa64
Zac Robinson <zachary.robinson@sky.uk>
2016-09-05 09:52:06 +0100

    Refactor command line interface

--- Main.py
+++ Main.py
@@ -12,28 +12,15 @@ def run(file):
         print(result[0])
 
     lex_result = lexer.lex(result[1])
-    print(const.HEADER_COLOUR + filename + const.END_COLOUR)
-    if lex_result["Status"] == "Error":
-        for error in lex_result["Tokens"]:
-            print(const.ERROR_COLOUR + "Error at line number:  " +
-                  str(error[1]) + const.END_COLOUR)
-
+    if lex_result["Status"] == "Error" or lex_result["Warnings"]:
+        print(const.HEADER_COLOUR + filename + const.END_COLOUR)
+        if lex_result["Status"] == "Error":
+            for error in lex_result["Tokens"]:
+                print(const.ERROR_COLOUR + str(error[0]) + "Line number: " + str(error[1]) + const.END_COLOUR)
         for warning in lex_result["Warnings"]:
             print(const.WARNING_COLOUR + str(warning) + const.END_COLOUR)
-        if not lex_result["Warnings"]:
-            print(const.PASS_COLOUR + "Lexed without warnings" + const.END_COLOUR)
-    else:
-        print(const.PASS_COLOUR + filename + "Lexed without errors" + const.END_COLOUR)
-        for warning in lex_result["Warnings"]:
-            print(const.WARNING_COLOUR + str(warning) + const.END_COLOUR)
-        if not lex_result["Warnings"]:
-            print(const.PASS_COLOUR + "Lexed without warnings" + const.END_COLOUR)
-
-    print("\n")
-
+        print("\n")
 
 if __name__ == '__main__':
     for filename in glob.iglob(sys.argv[1] + '/**/*.brs', recursive=True):
         run(filename)
-        # print(filename)
-        # run(sys.argv[1])
