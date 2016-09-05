71754e64250a4aa45fddf24041171ab7a7acdd5e
Jack Ingleton <jack.ingleton@sky.uk>
2016-09-05 15:13:54 +0100

    Spaces around operator refactor (#23)
    * Finished work to check for spaces around operators
    
    * Refactor setting white space count to 0
    
    * Refactored spaces around operator in lexer
    
    * Refactored tests for spaces around operators

--- src/Lexer.py
+++ src/Lexer.py
@@ -18,25 +18,28 @@ class Lexer:
         self.current_indentation_level = 0
         self.consecutive_empty_lines = 0
         self.is_empty_line = True
+        self.bslint_command_executor = src.BSLintCommandHandler(config)
         self.skip_styling = False
         self.skip_line_command_number = -1
         self.skip_file_styling = False
         self.white_space_count = 0
         self.on_operator = False
-        self.bslint_command_executor = src.BSLintCommandHandler(config)
+        self.all_characters = ""
+        self.spaces_around_operators_config = config['spaces_around_operators']["params"]['spaces_around_operators']
+        self.i = 0
 
     def lex(self, characters):
 
-        i = 0
+        self.all_characters = characters
         tokens = []
         errors = []
         self.skip_file_styling = False
 
-        while i < len(characters):
+        while self.i < len(characters):
             try:
-                match, token_type = self.regex_handler(characters[i:])
-                i += len(match.group())
-                last_line = re.findall("(?<=^).*", characters[:i], re.MULTILINE)
+                match, token_type = self.regex_handler(characters[self.i:])
+                self.i += len(match.group())
+                last_line = re.findall("(?<=^).*", characters[:self.i], re.MULTILINE)
                 if len(last_line) == 1:
                     last_line = last_line[-1]
                 else:
@@ -44,12 +47,11 @@ class Lexer:
                 tokens = self.match_handler(match, token_type, tokens, last_line)
 
             except ValueError:
-                end_of_line = re.match(r"(.*)\n", characters[i:])
-                errors.append(
-                    self.error.get(ErrConst.UNMATCHED_QUOTATION_MARK, [(end_of_line.group()[:-2]), self.line_number]))
+                end_of_line = re.match(r"(.*)\n", characters[self.i:])
+                errors.append(self.error.get(ErrConst.UNMATCHED_QUOTATION_MARK,
+                                                        [(end_of_line.group()[:-2]), self.line_number]))
                 self.line_number += 1
-                i += len(end_of_line.group())
-
+                self.i += len(end_of_line.group())
         if not self.skip_styling:
             self.warning_filter(self.execute_bslint_command('max_line_length',
                                                             {"line_length": self.line_length,
@@ -89,8 +91,6 @@ class Lexer:
 
     def apply_styling(self, match, token_type, characters):
         if token_type is const.NEW_LINE:
-            self.on_operator = False
-            self.white_space_count = 0
             if self.is_empty_line is True:
                 self.consecutive_empty_lines += 1
             else:
@@ -112,19 +112,11 @@ class Lexer:
                                                             {"empty_lines": self.consecutive_empty_lines,
                                                              "line_number": self.line_number}))
 
-        elif token_type is None:
-            self.white_space_count += 1
         else:
-            self.warning_filter(
-                self.execute_bslint_command('spaces_around_operators', {"line_number": self.line_number,
-                                                                        "white_space": self.white_space_count,
-                                                                        "on_operator": self.on_operator,
-                                                                        "operator": match.group()}))
-            self.on_operator = False
             self.is_empty_line = False
             if token_type == const.BSLINT_COMMAND:
-                self.execute_bslint_command(match.group('command'))
 
+                self.execute_bslint_command(match.group('command'))
             elif token_type == const.COMMENT:
                 self.warning_filter(self.execute_bslint_command('check_comment', {"token": match.group(),
                                                                                   "line_number": self.line_number}))
@@ -132,13 +124,13 @@ class Lexer:
                                                                                 "line_number": self.line_number,
                                                                                 "type": token_type}))
             elif token_type == const.OPERATOR:
-                self.on_operator = True
+                before_index = self.i - self.spaces_around_operators_config
+                after_index = self.i + self.spaces_around_operators_config
+
+                characters_around_operator = self.all_characters[before_index - 2:after_index + 1]
                 self.warning_filter(
                     self.execute_bslint_command('spaces_around_operators', {"line_number": self.line_number,
-                                                                            "white_space": self.white_space_count,
-                                                                            "on_operator": self.on_operator,
-                                                                            "operator": match.group()}))
-
+                                                                            "characters": characters_around_operator}))
             elif token_type == const.ID:
                 self.warning_filter(
                     self.execute_bslint_command('spell_check',
@@ -147,9 +139,6 @@ class Lexer:
             elif token_type == const.PRINT_KEYWORD:
                 self.warning_filter(
                     self.execute_bslint_command('check_trace_free', {"line_number": self.line_number}))
-
-            self.white_space_count = 0
-
     def regex_handler(self, characters):
         for regex in regexs.List:
             match = re.match(regex[0], characters, re.IGNORECASE)
