02d7a5a0a77118b9726a533e9f86d7860f73ca1d
Zac Robinson <zacrobinson1992@gmail.com>
2016-09-05 11:29:02 +0100

    Command line spike (#22)
    Merge command line interface into dev

--- src/FileReader.py
+++ src/FileReader.py
@@ -2,17 +2,14 @@ import src
 
 
 class FileReader:
-
     @staticmethod
-    def lex_file(config_file, file_to_lex):
-        config_json = src.read_json(config_file)
+    def read_file(file_to_lex):
+        config_json = src.load_config_file()
+        # print(config_json)
 
         fo = open(file_to_lex, "r+")
         str_to_lex = fo.read()
 
         bslint_command_executor = src.BSLintCommandHandler(config_json)
-        bslint_command_executor.execute_bslint_command("check_file_encoding", {"file_path": file_to_lex})
-        lexer = src.Lexer(config_json)
-
-
-        lexer.lex(str_to_lex)
+        return bslint_command_executor.execute_bslint_command("check_file_encoding",
+                                                              {"file_path": file_to_lex}), str_to_lex, config_json
