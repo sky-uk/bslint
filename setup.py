f80a0523186f5a77fa0d3eda4f31b1738e6e0a9f
JackIngleton <jack.ingleton@sky.uk>
2016-09-09 16:30:53 +0100

    Fixed configurable spellcheck for pip install

--- setup.py
+++ setup.py
@@ -19,18 +19,15 @@ with open("README.rst", "rb") as f:
 pip.main(['install', 'pyenchant'])
 
 setup(
-    name="bslint",
-    packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
-    package_dir={'src': 'src'},
-    package_data={'src': ['config/*.json']},
-    entry_points={"console_scripts": ['bslint = src.bslint:main']},
-    version=version,
-    description="A linter tool for the BrightScript language.",
-    long_description=long_descr,
-    author="BSLint",
-    author_email="zachary.robinson@sky.uk",
-    url="https://github.com/sky-uk/bslint",
-    download_url='https://github.com/sky-uk/bslint/archive/0.2.4.tar.gz',
-    install_requires=['pip']
-)
 
+    name = "bslint",
+    packages = find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
+    package_data={'src': ['config/*.json', 'config/personal-words-list.txt']},
+    entry_points = { "console_scripts": ['bslint = src.bslint:main'] },
+    version = version,
+    description = "A linter tool for the BrightScript language.",
+    long_description = long_descr,
+    author = "BSLint",
+    author_email = "",
+    url = "https://github.com/sky-uk/bslint",
+    )
