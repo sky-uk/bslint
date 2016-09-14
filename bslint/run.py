37e839352eb20049893f0308547b0784d18a0263
Shane Bloomer <shane.bloomer@sky.uk>
2016-09-14 16:07:55 +0100

    Removed unused run.py

--- bslint/run.py
+++
@@ -1,6 +0,0 @@
-import bslint
-
-def run():
-    config = bslint.load_config_file(user='SpellCheck/spellcheck-config.json', default='test-config.json')
-    lexer = bslint.Lexer(config)
-    lexer.lex("sdafsb ")
