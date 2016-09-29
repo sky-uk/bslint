e923648832609b8b632c4774a9154634cde6884f
sbsky <shane.bloomer@sky.uk>
2016-09-29 16:11:09 +0100

    Changed regex list of tuples to named_tuples. (#59)

--- bslint/utilities/regex_handler.py
+++ bslint/utilities/regex_handler.py
@@ -3,10 +3,10 @@ import re
 
 
 def find_match(characters):
-    for regex in regexs.List:
-        match = re.match(regex[0], characters, re.IGNORECASE)
+    for token in regexs.List:
+        match = re.match(token.regex, characters, re.IGNORECASE)
         if match:
             break
     if not match:
         raise ValueError('NO MATCH FOUND')
-    return {"match": match, "token_type": regex[1], "indentation_level": regex[2]}
+    return {"match": match, "token_type": token.type, "indentation_level": token.indentation}
