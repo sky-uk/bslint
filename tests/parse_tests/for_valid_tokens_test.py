3b063c12356d3336cc03d3c55f87de30003d87e3
JackIngleton <jack.ingleton@sky.uk>
2016-10-03 16:57:59 +0100

    Commented out unfinished equals tests

--- tests/parse_tests/for_valid_tokens_test.py
+++ tests/parse_tests/for_valid_tokens_test.py
@@ -1,40 +1,40 @@
-import unittest
-from bslint import constants as const
-from bslint.parser.parser import Parser
-
-
-class TestEqualsValidTokens(unittest.TestCase):
-
-    @classmethod
-    def setUpClass(cls):
-        cls.preceding_token_type = const.EQUALS
-
-    def testID(self):
-        current_token_type = const.ID
-        result = Parser.is_valid_token(self.preceding_token_type, current_token_type)
-        self.assertTrue(result)
-
-    def testValue(self):
-        current_token_type = const.VALUE
-        result = Parser.is_valid_token(self.preceding_token_type, current_token_type)
-        self.assertTrue(result)
-
-    def testInvalid(self):
-        current_token_type = const.INVALID
-        result = Parser.is_valid_token(self.preceding_token_type, current_token_type)
-        self.assertTrue(result)
-
-    def testAs(self):
-        current_token_type = const.AS
-        result = Parser.is_valid_token(self.preceding_token_type, current_token_type)
-        self.assertFalse(result)
-
-    def testFor(self):
-        current_token_type = const.FOR
-        result = Parser.is_valid_token(self.preceding_token_type, current_token_type)
-        self.assertFalse(result)
-
-    def testCloseSquareBracket(self):
-        current_token_type = const.CLOSE_SQUARE_BRACKET
-        result = Parser.is_valid_token(self.preceding_token_type, current_token_type)
-        self.assertFalse(result)
+# import unittest
+# from bslint import constants as const
+# from bslint.parser.parser import Parser
+#
+#
+# class TestEqualsValidTokens(unittest.TestCase):
+#
+#     @classmethod
+#     def setUpClass(cls):
+#         cls.preceding_token_type = const.EQUALS
+#
+#     def testID(self):
+#         current_token_type = const.ID
+#         result = Parser.is_valid_token(self.preceding_token_type, current_token_type)
+#         self.assertTrue(result)
+#
+#     def testValue(self):
+#         current_token_type = const.VALUE
+#         result = Parser.is_valid_token(self.preceding_token_type, current_token_type)
+#         self.assertTrue(result)
+#
+#     def testInvalid(self):
+#         current_token_type = const.INVALID
+#         result = Parser.is_valid_token(self.preceding_token_type, current_token_type)
+#         self.assertTrue(result)
+#
+#     def testAs(self):
+#         current_token_type = const.AS
+#         result = Parser.is_valid_token(self.preceding_token_type, current_token_type)
+#         self.assertFalse(result)
+#
+#     def testFor(self):
+#         current_token_type = const.FOR
+#         result = Parser.is_valid_token(self.preceding_token_type, current_token_type)
+#         self.assertFalse(result)
+#
+#     def testCloseSquareBracket(self):
+#         current_token_type = const.CLOSE_SQUARE_BRACKET
+#         result = Parser.is_valid_token(self.preceding_token_type, current_token_type)
+#         self.assertFalse(result)
