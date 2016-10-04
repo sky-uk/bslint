cb8a4a26ab15746c6513cac6b5827bfdedf68df8
sbsky <shane.bloomer@sky.uk>
2016-10-04 09:50:02 +0100

    Added extra BrightScript keywords and operators (#60)
    * Changed regex list of tuples to named_tuples. (#59)
    
    * Added curly braces to regexes list and created unit tests
    
    * Added missing BrightScript keywords and symbols

--- tests/RegexTests/misc_regex_test.py
+++ tests/RegexTests/misc_regex_test.py
@@ -43,3 +43,40 @@ class TestMiscRegex(unittest.TestCase):
         result = regex_handler.find_match(identifier)
         self.assertEqual(result["match"].group(), identifier)
         self.assertEqual(result["token_type"], const.SQUARE_BRACKET)
+
+    def testOpenCurlyBracket(self):
+        identifier = "{"
+        result = regex_handler.find_match(identifier)
+        self.assertEqual(result["match"].group(), identifier)
+        self.assertEqual(result["token_type"], const.CURLY_BRACKET)
+
+    def testCloseCurlyBracket(self):
+        identifier = "}"
+        result = regex_handler.find_match(identifier)
+        self.assertEqual(result["match"].group(), identifier)
+        self.assertEqual(result["token_type"], const.CURLY_BRACKET)
+
+    def testCloseCurlyBracket(self):
+        identifier = "}"
+        result = regex_handler.find_match(identifier)
+        self.assertEqual(result["match"].group(), identifier)
+        self.assertEqual(result["token_type"], const.CURLY_BRACKET)
+
+    def testSemiColon(self):
+        identifier = ";"
+        result = regex_handler.find_match(identifier)
+        self.assertEqual(result["match"].group(), identifier)
+        self.assertEqual(result["token_type"], const.SEMI_COLON)
+
+    def testAtSymbol(self):
+        identifier = "@"
+        result = regex_handler.find_match(identifier)
+        self.assertEqual(result["match"].group(), identifier)
+        self.assertEqual(result["token_type"], const.AT_SYMBOL)
+
+    def testHashSymbol(self):
+        identifier = "#"
+        result = regex_handler.find_match(identifier)
+        self.assertEqual(result["match"].group(), identifier)
+        self.assertEqual(result["token_type"], const.HASH_SYMBOL)
+
